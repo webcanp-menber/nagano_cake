7b4db4373b32e54bf20d8d5d8aaceecb5eb38322
EC2 Default User <ec2-user@ip-172-31-27-47.ap-northeast-1.compute.internal>
2024-01-23 05:06:07 +0000

    [UPDATE]controllers

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,17 +3,18 @@
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  #GET /resource/sign_in
+  def new
+    #super
+    render 'admin/sessions/new'
+  end
 
-  # POST /resource/sign_in
+  #POST /resource/sign_in
   # def create
   #   super
   # end
 
-  # DELETE /resource/sign_out
+  #DELETE /resource/sign_out
   # def destroy
   #   super
   # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
@@ -6,5 +7,34 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  scope module: :public do
+    root to:"homes#top"
+    resources :items, only: [:index, :show]
+    resources :registrations, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :customers, only: [:show, :edit, :update]
+    resources :cart_items, only: [:index, :update, :destory, :create]
+    resources :orders, only: [:new, :create, :index, :show]
+
+    get "/about" => 'homes#about'
+    get "/customers/unsubscribe" => 'customers#unsubscribe'
+    patch "/customers/withdraw" => 'customers#withdraw'
+    delete "/cart_items/destroy_all" => 'cart_items#destroy_all'
+    post "/orders/confirm" => 'orders#confirm'
+    get "/orders/complete" => 'orders#complere'
+
+  end
+
+
+  namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :items, only: [:new, :index, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show]
+    get "/admin/top" => 'sessions#top'
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
