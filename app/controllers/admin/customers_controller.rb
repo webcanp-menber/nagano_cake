9ee7b97cc151777e5272f75ef5cc0876a48cb663
EC2 Default User <ec2-user@ip-172-31-27-47.ap-northeast-1.compute.internal>
2024-01-28 10:27:01 +0000

    [NEW]会員管理

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,32 @@
 class Admin::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path
+    else
+      render :edit
+    end
+  end
+
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number)
+  end
+
 end
+
---
+++ app/controllers/public/items_controller.rb
@@ -0,0 +1,16 @@
+class Public::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price)
+  end
+
+end
